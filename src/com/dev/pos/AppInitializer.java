07c671c2aafb2b4b800f06e9d3810b0f13fd3933
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-03 17:42:33 +0530

    add setUI method after load screen then pop up Login form

--- src/com/dev/pos/AppInitializer.java
+++ src/com/dev/pos/AppInitializer.java
@@ -13,7 +13,8 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("./view/splash/SplashScreen.fxml"))));
+//        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/SplashScreen.fxml"))));
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/LoginForm.fxml"))));
         primaryStage.show();
         primaryStage.centerOnScreen();
     }
--- src/com/dev/pos/controller/SplashScreenController.java
+++ src/com/dev/pos/controller/SplashScreenController.java
@@ -1,15 +1,22 @@
 package com.dev.pos.controller;
 
 import com.dev.pos.util.splash.LoadingTask;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SplashScreenController {
     public Rectangle rectangleMain;
     public Rectangle rectangleSub;
     public Label lblProgress;
+    public AnchorPane context;
 
-    public void initialize() {
+    public void initialize() throws IOException {
         LoadingTask task = new LoadingTask();
 
         task.progressProperty().addListener((observable, oldValue, newValue) -> {
@@ -23,6 +30,21 @@ public class SplashScreenController {
 
         new Thread(task).start();
 
+        task.setOnSucceeded(event -> {
+            try {
+                setUI("LoginForm");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+
+    private void setUI(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/" + location + ".fxml"))));
+        stage.show();
+        stage.centerOnScreen();
     }
 
 }
